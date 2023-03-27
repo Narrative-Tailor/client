d55c8cb70bb57ece69fb02b40d6fb653b976b352
은승균 <dmstmdrbs98@gmail.com>
2023-03-27 23:57:20 +0900

    feat: 스낵바 프로바이더

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,11 @@
-import "@styles/global.css";
 import type {AppProps} from "next/app";
+import "@styles/global.css";
+import SnackbarProvider from "react-simple-snackbar";
 
 export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <SnackbarProvider>
+      <Component {...pageProps} />
+    </SnackbarProvider>
+  );
 }
