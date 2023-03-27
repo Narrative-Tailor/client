f467380342f1e986feb384d027f7a0779970fc51
은승균 <dmstmdrbs98@gmail.com>
2023-03-27 23:58:23 +0900

    feat: 데이터 타입 및 목데이터 추가

---
+++ src/constants/novels.ts
@@ -0,0 +1,24 @@
+import {Novel} from "@models/novel";
+
+export const mockNovels: Novel[] = [
+  {
+    id: 1,
+    title: "소설 1",
+    description: "소설 1은 이런 내용입니다.",
+  },
+  {
+    id: 2,
+    title: "소설 2",
+    description: "소설 2는 이런 내용입니다.",
+  },
+  {
+    id: 3,
+    title: "소설 3",
+    description: "소설 3은 이런 내용입니다.",
+  },
+  {
+    id: 4,
+    title: "소설 4",
+    description: "소설 4는 이런 내용입니다.",
+  },
+];
---
+++ src/models/novel.ts
@@ -0,0 +1,5 @@
+export type Novel = {
+  id: number;
+  title: string;
+  description?: string;
+};
